be39e7262294b7ea42bf25ce23cb6c53f3514f0b
rsek <hello@rsek.ca>
2022-08-11 20:41:10 -0700

    restructure classic pc sheet w/ new layout

--- src/module/vue/character-sheet.vue
+++ src/module/vue/character-sheet.vue
@@ -1,103 +1,24 @@
 <template>
-  <div class="flexcol">
-    <!-- Header row -->
-    <CharacterHeader />
-
-    <!-- Main body row -->
-    <div class="flexrow">
-      <!-- Momentum on left -->
-      <div class="flexcol margin-left">
-        <MomentumMeterSlider :actor="actor" orientation="vertical">
-        </MomentumMeterSlider>
-      </div>
-
-      <!-- Center area -->
-      <div class="flexcol">
-        <!-- Attributes -->
-        <div class="flexrow stats">
-          <AttrBox :actor="actor" attr="edge"></AttrBox>
-          <AttrBox :actor="actor" attr="heart"></AttrBox>
-          <AttrBox :actor="actor" attr="iron"></AttrBox>
-          <AttrBox :actor="actor" attr="shadow"></AttrBox>
-          <AttrBox :actor="actor" attr="wits"></AttrBox>
-        </div>
-
-        <Tabs style="margin-top: 0.5rem">
-          <Tab :title="$t('IRONSWORN.Character')"><IronswornMain /></Tab>
-          <Tab :title="$t('IRONSWORN.Notes')"><IronswornNotes /></Tab>
-        </Tabs>
-
-        <!-- Conditions & Banes & Burdens -->
-        <section class="sheet-area nogrow">
-          <Conditions />
-        </section>
-      </div>
-
-      <!-- Stats on right -->
-      <div class="flexcol margin-right">
-        <div class="flexrow nogrow" style="flex-wrap: nowrap">
-          <!-- TODO: restyle as h4-like -->
-          <BtnRollStat
-            class="nogrow vertical-v2 text"
-            :actor="actor"
-            attr="health"
-          >
-            {{ $t('IRONSWORN.Health') }}
-          </BtnRollStat>
-          <div class="flexcol stack health">
-            <ConditionMeterSlider
-              :actor="actor"
-              attr="health"
-              :max="5"
-              :min="0"
-            ></ConditionMeterSlider>
-          </div>
-        </div>
-
-        <hr class="nogrow" />
-
-        <div class="flexrow nogrow" style="flex-wrap: nowrap">
-          <!-- TODO: restyle as h4-like -->
-          <BtnRollStat
-            class="nogrow vertical-v2 text"
-            :actor="actor"
-            attr="spirit"
-          >
-            {{ $t('IRONSWORN.Spirit') }}
-          </BtnRollStat>
-          <div class="flexcol stack spirit">
-            <ConditionMeterSlider
-              :actor="actor"
-              attr="spirit"
-              :max="5"
-              :min="0"
-            ></ConditionMeterSlider>
-          </div>
-        </div>
-
-        <hr class="nogrow" />
-
-        <div class="flexrow nogrow" style="flex-wrap: nowrap">
-          <!-- TODO: restyle as h4-like -->
-          <BtnRollStat
-            class="nogrow vertical-v2 text"
-            :actor="actor"
-            attr="supply"
-          >
-            {{ $t('IRONSWORN.Supply') }}
-          </BtnRollStat>
-          <div class="flexcol stack supply">
-            <ConditionMeterSlider
-              :actor="actor"
-              attr="supply"
-              :max="5"
-              :min="0"
-            ></ConditionMeterSlider>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <SheetLayoutSidebars class="classic-character-sheet">
+    <template #header>
+      <DocumentImg :document="actor" />
+      <DocumentName :document="actor" />
+      <XpTrackerClassic />
+    </template>
+    <template #default>
+      <Tabs>
+        <Tab :title="$t('IRONSWORN.Character')">
+          <IronswornMain />
+        </Tab>
+        <Tab :title="$t('IRONSWORN.Notes')">
+          <IronswornNotes />
+        </Tab>
+      </Tabs>
+    </template>
+    <template #footer>
+      <Conditions />
+    </template>
+  </SheetLayoutSidebars>
 </template>
 
 <style lang="less" scoped>
@@ -108,20 +29,18 @@
 </style>
 
 <script setup lang="ts">
+import SheetLayoutSidebars from './components/layout/SheetLayoutSidebars.vue'
 import { $ActorKey } from './provisions'
-import AttrBox from './components/attr-box.vue'
-import BtnRollStat from './components/buttons/btn-roll-stat.vue'
 import { IronswornActor } from '../actor/actor'
 import { provide, computed, inject } from 'vue'
-import { RollDialog } from '../helpers/rolldialog'
-import CharacterHeader from './components/character-header.vue'
 import Conditions from './components/conditions/conditions.vue'
 import Tabs from './components/tabs/tabs.vue'
 import Tab from './components/tabs/tab.vue'
 import IronswornMain from './components/character-sheet-tabs/ironsworn-main.vue'
 import IronswornNotes from './components/character-sheet-tabs/ironsworn-notes.vue'
-import MomentumMeterSlider from './components/resource-meters/momentum-meter-slider.vue'
-import ConditionMeterSlider from './components/resource-meters/condition-meter-slider.vue'
+import DocumentImg from './components/document-img.vue'
+import DocumentName from './components/document-name.vue'
+import XpTrackerClassic from './components/xp-tracker-classic.vue'
 
 const props = defineProps<{
   actor: ReturnType<typeof IronswornActor.prototype.toObject>
@@ -133,12 +52,4 @@ provide(
 )
 
 const $actor = inject($ActorKey)
-
-function rollStat(stat) {
-  RollDialog.show({ actor: $actor, stat })
-}
-function openCompendium(name) {
-  const pack = game.packs?.get(`foundry-ironsworn.${name}`)
-  pack?.render(true)
-}
 </script>
