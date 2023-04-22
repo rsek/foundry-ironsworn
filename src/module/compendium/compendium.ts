f3fe7986199c6b61cd90a5cb15527f942fdffd1b
rsek <5354757+rsek@users.noreply.github.com>
2023-04-22 03:45:18 -0700

    rewrite as namespace, the class isn't configurable

--- src/module/compendium/compendium.ts
+++ src/module/compendium/compendium.ts
@@ -1,9 +1,7 @@
 import type { DocumentClassForCompendiumMetadata } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/collections/compendium'
 
-export class IronCompendiumCollection<
-	T extends CompendiumCollection.Metadata
-> extends CompendiumCollection<T> {
-	static readonly CANONICAL_PACKS = {
+export namespace CompendiumCollection {
+	export const CANONICAL_PACKS = {
 		Actor: ['foundry-ironsworn.foeactorssf', 'foundry-ironsworn.foeactorsis'],
 		Item: [
 			'foundry-ironsworn.starforgedassets',
@@ -25,23 +23,22 @@ export class IronCompendiumCollection<
 		]
 	} as const
 
-	get canonical() {
-		return IronCompendiumCollection.CANONICAL_PACKS[
-			this.documentName as any
-		]?.includes(this.collection as any)
-	}
-
-	hasDfId(dfid: string) {
-		throw new Error('NYI')
-	}
-
-	empty() {
-		for (const item of this) {
-			this.index.delete(item.id)
-			this.delete(item.id)
+	export function empty<T extends CompendiumCollection<any>>(pack: T) {
+		for (const item of pack) {
+			pack.index.delete(item.id)
+			pack.delete(item.id)
 		}
 	}
-}
 
-// could the compendium get some flags that marks it for use as tree data?
-// for example, keeping: a list of all folders?
+	export function isCanonical<T extends CompendiumCollection<any>>(pack: T) {
+		return Boolean(
+			CANONICAL_PACKS[pack.documentName as any]?.includes(
+				pack.collection as any
+			)
+		)
+	}
+
+	export function hasDfId(dfid: string) {
+		throw new Error('NYI')
+	}
+}
